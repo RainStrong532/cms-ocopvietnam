cce6605983b19c01851b77ea183d1021505e2c5c
manh van vu <vuvanmanhttvtvp@gmail.com>
2021-02-08 00:00:39 +0700

    add meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,20 @@ import "./styles/app.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react'
 import { AuthProvider, ProtectRoute } from "../src/context/auth";
+import header from 'next/head'
 
-function MyApp({ Component, pageProps}) {
+function MyApp({ Component, pageProps }) {
 
-    return (
-      <AuthProvider>
-        <ProtectRoute>
-            <Component {...pageProps} />
-        </ProtectRoute>
-      </AuthProvider>
-    )
+  return (
+    <AuthProvider>
+      <ProtectRoute>
+        <header>
+          <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+        </header>
+        <Component {...pageProps} />
+      </ProtectRoute>
+    </AuthProvider>
+  )
 }
 
 
