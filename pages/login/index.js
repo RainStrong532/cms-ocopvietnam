5e3df7f0a8cb55519006068e7a55b4e5236725d1
manh van vu <vuvanmanhttvtvp@gmail.com>
2021-02-07 23:42:45 +0700

    final final commit

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Products from '../views/components/Products';
-import Layout from '../views/components/Layout';
-import useAuth,{ ProtectRoute } from '../context/auth'
 import Login from '../views/containers/Login';
 
 function login() {
